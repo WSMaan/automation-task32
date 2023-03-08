7e3fdffdbb864dc6c73a0c19f25058eeeadf9f32
Maan9 <wsmaan896@gmail.com>
2023-03-08 18:40:14 +0000

    Test 4 | Select a model from the model selection page and confirm the personalised quote page has loaded. Change the dropdown value for the Engine Dropdown from the default engine selected.

--- src/main/java/com/connect_group/tests/financecalculator/FinanceCalculatorTest.java
+++ src/main/java/com/connect_group/tests/financecalculator/FinanceCalculatorTest.java
@@ -7,6 +7,8 @@ import com.connect_group.tests.BaseTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import java.net.URL;
 
@@ -14,38 +16,46 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FinanceCalculatorTest extends BaseTest {
 
-  private ModelSelectPage modelSelectPage;
-  private FilterByPage filterByPage;
-  private PersonalisedQuotePage personalisedQuotePage;
+    private ModelSelectPage modelSelectPage;
+    private FilterByPage filterByPage;
+    private PersonalisedQuotePage personalisedQuotePage;
 
-  @BeforeEach
-  void setupAndNavigateToUrl() {
-    modelSelectPage = new ModelSelectPage(driver);
-    filterByPage = new FilterByPage(driver);
-    personalisedQuotePage = new PersonalisedQuotePage(driver);
+    @BeforeEach
+    void setupAndNavigateToUrl() {
+        modelSelectPage = new ModelSelectPage(driver);
+        filterByPage = new FilterByPage(driver);
+        personalisedQuotePage = new PersonalisedQuotePage(driver);
 
-    open("https://www.landrover.co.uk/offers-and-finance/finance-calculator.html");
-  }
+        open("https://www.landrover.co.uk/offers-and-finance/finance-calculator.html");
+    }
 
-  @Test
-  void ensureModelSelectPageIsPresent() {
-    assertTrue(modelSelectPage.isInitialized());
-  }
+    @Test
+    void ensureModelSelectPageIsPresent() {
+        assertTrue(modelSelectPage.isInitialized());
+    }
 
-  // TODO: Complete the each of the tests under the "Tasks to Complete" section of the README.md
-
-
-  @Test
-  void selectAndChangeVehicle() {
-
-    driver.findElement(By.xpath("(//div[@class='fc-nameplate__imageOverlay'])[2]")).click();
-    driver.getCurrentUrl().equals("https://www.landrover.co.uk/offers-and-finance/finance-calculator.html#/quote/l461_k23__4a8k7__a-d300_a-std_a-swb_sp2");
-  driver.findElement(By.xpath("//span[normalize-space()='Change Vehicle']")).click();
-
-  driver.findElement(By.xpath("//div[@class='QuickQuote']//div[2]//div[1]//div[1]//a[1]//span[1]//div[1]")).click();
-  driver.getCurrentUrl().equals("https://www.landrover.co.uk/offers-and-finance/finance-calculator.html#/quote/l551_k235__4a8js__a-d163-m-fwd_a-evoque_a-swb_sp0");
+    // TODO: Complete the each of the tests under the "Tasks to Complete" section of the README.md
 
 
+//  @Test
+//  void selectAndChangeVehicle() {
+//
+//    driver.getCurrentUrl().equals("https://www.landrover.co.uk/offers-and-finance/finance-calculator.html#/quote/l461_k23__4a8k7__a-d300_a-std_a-swb_sp2");
+//  driver.findElement(By.xpath("//span[normalize-space()='Change Vehicle']")).click();
+//
+//  driver.findElement(By.xpath("//div[@class='QuickQuote']//div[2]//div[1]//div[1]//a[1]//span[1]//div[1]")).click();
+//  driver.getCurrentUrl().equals("https://www.landrover.co.uk/offers-and-finance/finance-calculator.html#/quote/l551_k235__4a8js__a-d163-m-fwd_a-evoque_a-swb_sp0");
 
-  }
+
+    //}
+    @Test
+    void changeEngineDropDown() {
+        driver.findElement(By.xpath("//div[@role='main']//div[3]//div[1]//div[1]//a[1]//span[1]//div[1]")).click();
+//  Select engineDropDown =new Select(driver.findElement(By.xpath("//button[@id='dropdown__button-engines']")));
+//    engineDropDown.selectByVisibleText("");
+
+        driver.findElement(By.xpath("//button[@id='dropdown__button-engines']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='P250 Petrol']")).click();
+        driver.findElement(By.xpath("//span[@class='fc-cta__cta--size-primary']")).click();
+    }
 }
