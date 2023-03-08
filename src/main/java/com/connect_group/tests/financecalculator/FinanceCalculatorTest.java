a4bb45380bf36873cafefe8aece3ff21f909d626
Maan9 <wsmaan896@gmail.com>
2023-03-08 17:45:04 +0000

    Test 3 | Select a model from the model selection page and confirm the personalised quote page has loaded. Click on the change vehicle button and ensure you are returned to the model selection page and can select a different nameplate

--- src/main/java/com/connect_group/tests/financecalculator/FinanceCalculatorTest.java
+++ src/main/java/com/connect_group/tests/financecalculator/FinanceCalculatorTest.java
@@ -6,6 +6,9 @@ import com.connect_group.pages.financecalculator.PersonalisedQuotePage;
 import com.connect_group.tests.BaseTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -30,4 +33,19 @@ class FinanceCalculatorTest extends BaseTest {
   }
 
   // TODO: Complete the each of the tests under the "Tasks to Complete" section of the README.md
+
+
+  @Test
+  void selectAndChangeVehicle() {
+
+    driver.findElement(By.xpath("(//div[@class='fc-nameplate__imageOverlay'])[2]")).click();
+    driver.getCurrentUrl().equals("https://www.landrover.co.uk/offers-and-finance/finance-calculator.html#/quote/l461_k23__4a8k7__a-d300_a-std_a-swb_sp2");
+  driver.findElement(By.xpath("//span[normalize-space()='Change Vehicle']")).click();
+
+  driver.findElement(By.xpath("//div[@class='QuickQuote']//div[2]//div[1]//div[1]//a[1]//span[1]//div[1]")).click();
+  driver.getCurrentUrl().equals("https://www.landrover.co.uk/offers-and-finance/finance-calculator.html#/quote/l551_k235__4a8js__a-d163-m-fwd_a-evoque_a-swb_sp0");
+
+
+
+  }
 }
